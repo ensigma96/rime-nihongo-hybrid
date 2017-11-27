69cee7bdb86c7426574403243550677a97a58dc1
Yunyang Liu <ensigma96@gmail.com>
2017-11-27 10:20:00 +0800

    Add opencc config t2jp.json for kyuujitai -> shinjitai conversion.

--- main.sh
+++ main.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+source './main.conf'
+
 readonly DIR=`pwd`
 readonly SRC="${DIR}/src"
 readonly STUB="${SRC}/stub"
+readonly DICT="${STUB}/dict"
+readonly SCHEMA="${STUB}/schema"
+readonly OPENCC="${STUB}/opencc"
 readonly DATA="${SRC}/data"
 readonly CONVERTER="${SRC}/converter"
 readonly MOZC_DICT_DIR="${DATA}/mozc_dictionaries"
@@ -10,5 +15,11 @@ readonly DIST="${DIR}/dist"
 
 rm -rf "${DIST}"
 mkdir -p "${DIST}"
-cp -r "${STUB}/." "${DIST}"
+cp -r "${DICT}/." "${DIST}"
+if [[ "${OPENCC_LEGACY_MODE}" -eq $zero ]]; then
+	cp -r "${OPENCC}" "${DIST}"
+	cp "${SCHEMA}/nihongo-hybrid.schema.yaml" "${DIST}"
+else
+	cp "${SCHEMA}/nihongo-hybrid.legacy.schema.yaml" "${DIST}/nihongo-hybrid.schema.yaml"
+fi
 python3 "${CONVERTER}/main.py" ${MOZC_DICT_DIR} >> "${DIST}/nihongo-hybrid.dict/nihongo-hybrid.mozc.dict.yaml"
