43e534a4ae8a63960307bd5256badf805e520472
Yunyang Liu <ensigma96@gmail.com>
2017-12-21 20:46:31 +0800

    Build opencc dict from text instead of using a prebuilt one; modify main.sh

--- main.sh
+++ main.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
 readonly DIR=`pwd`
+
 readonly SRC="${DIR}/src"
 readonly STUB="${SRC}/stub"
 readonly DICT="${STUB}/dict"
 readonly SCHEMA="${STUB}/schema"
 readonly OPENCC="${STUB}/opencc"
 readonly CONVERTER="${SRC}/converter"
-readonly DIST="${DIR}/dist"
 
 readonly THIRD_PARTY="${DIR}/third_party"
 readonly TP_DATA="third_party/data"
-readonly OPENCC_DATA="${TP_DATA}/opencc"
+readonly OPENCC_JPVARIANTS_TEXT="${TP_DATA}/opencc/JPVariants.txt"
 readonly MOZC_DICT_DIR="${TP_DATA}/mozc_dictionaries"
 readonly JMDICT_E="${TP_DATA}/JMdict/JMdict_e.xml"
 
+readonly DIST="${DIR}/dist"
+
 rm -rf "${DIST}"
 mkdir -p "${DIST}"
-cp -r "${DICT}/." "${DIST}"
-cp -r "${OPENCC}" "${DIST}"
-cp -r "${OPENCC_DATA}" "${DIST}"
-cp "${SCHEMA}/nihongo-hybrid.schema.yaml" "${DIST}"
-python3 "${CONVERTER}/mozc.py" ${MOZC_DICT_DIR} >> "${DIST}/nihongo-hybrid.dict/nihongo-hybrid.mozc.dict.yaml"
-python3 "${CONVERTER}/jmdict.py" ${JMDICT_E} >> "${DIST}/nihongo-hybrid.dict/nihongo-hybrid.jmdict.dict.yaml"
+cp -r "${DICT}/." "${DIST}/"
+cp -r "${OPENCC}/." "${DIST}/opencc/"
+opencc_dict -i "${OPENCC_JPVARIANTS_TEXT}" -o "${DIST}/opencc/JPVariants.ocd" -f text -t ocd
+cp "${SCHEMA}/nihongo-hybrid.schema.yaml" "${DIST}/"
+python3 "${CONVERTER}/mozc.py" "${MOZC_DICT_DIR}" >> "${DIST}/nihongo-hybrid.dict/nihongo-hybrid.mozc.dict.yaml"
+python3 "${CONVERTER}/jmdict.py" "${JMDICT_E}" >> "${DIST}/nihongo-hybrid.dict/nihongo-hybrid.jmdict.dict.yaml"
 python3 "${CONVERTER}/kana.py" >> "${DIST}/nihongo-hybrid.dict/nihongo-hybrid.kana.dict.yaml"
