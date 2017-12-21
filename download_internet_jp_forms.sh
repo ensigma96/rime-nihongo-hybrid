e60eccbd524ddd48d8de6dbfb823cc8a7b241d4a
Yunyang Liu <ensigma96@gmail.com>
2017-12-21 13:49:52 +0800

    Move third party stuff to `third_party/`

--- download_internet_jp_forms.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-readonly INTERNET_JP_FORMS_URI='http://corpus.leeds.ac.uk/frqc/internet-jp-forms.num'
-readonly DIR=`pwd`
-readonly INTERNET_JP_FORMS="${DIR}/src/data/internet-jp-forms.num"
-
-wget -O - "${INTERNET_JP_FORMS_URI}" > ${INTERNET_JP_FORMS}
--- download_jmdict_e.sh
+++ download_jmdict_e.sh
@@ -2,7 +2,7 @@
 
 readonly JMDICT_E_URI='ftp://ftp.monash.edu.au/pub/nihongo/JMdict_e.gz'
 readonly DIR=`pwd`
-readonly JMDICT_DIR="${DIR}/src/data/JMdict"
+readonly JMDICT_DIR="${DIR}/third_party/data/JMdict"
 readonly JMDICT_E="${JMDICT_DIR}/JMdict_e.xml"
 
 wget -O - "${JMDICT_E_URI}" | gzip -d > "${JMDICT_E}"
